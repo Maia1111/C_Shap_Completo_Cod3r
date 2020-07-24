7aba18be19c01dfa6d0a7729e50535252c9c5702
Rogerio Maia <rogerioreservadosinop@gmail.com>
2020-07-24 10:44:25 -0400

    Primeiro commit com todos os arquivos iniciais prontos

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+    }
+}
---
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CursoCSharp;
+using CursoCSharp.Fundamentos;
+using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // Fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
